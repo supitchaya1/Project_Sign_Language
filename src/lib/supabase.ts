3144cc4ff27f12ce2ffc6cbcbe3fa9b31a4251a4
Pinky <supitchaya.ana@student.mahidol.edu>
2026-04-09 18:12:43 +0700

    edit code supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,22 @@
+// import { createClient } from "@supabase/supabase-js";
+
+// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+// const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+// if (!supabaseUrl || !supabaseAnonKey) {
+//   throw new Error("Missing VITE_SUPABASE_URL or VITE_SUPABASE_ANON_KEY");
+// }
+
+// export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
+  console.log("VITE_SUPABASE_URL =", supabaseUrl);
+  console.log("VITE_SUPABASE_ANON_KEY =", supabaseAnonKey);
   throw new Error("Missing VITE_SUPABASE_URL or VITE_SUPABASE_ANON_KEY");
 }
 
